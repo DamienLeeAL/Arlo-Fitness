f6ce133d82a8dabf37f56163dc61558d0d086546
DamienLeeAL <89609534+DamienLeeAL@users.noreply.github.com>
2026-09-18 10:16:27 -0500

    Add files via upload

---
+++ sw[1].js
@@ -0,0 +1,4 @@
+const CACHE='arlo-fitness-v1';const CORE=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r}).catch(()=>c)))});
